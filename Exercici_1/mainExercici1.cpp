ae16c897c79e8f1e566e8aeb37c3755132533d0d
Joanfmir <119803882+Joanfmir@users.noreply.github.com>
2026-02-10 09:59:54 +0100

    1er ex

--- Exercici_1/mainExercici1.cpp
+++ Exercici_1/mainExercici1.cpp
@@ -8,30 +8,37 @@ int main(){
     string nom;
     cout << "Introdueix el teu nom: " << endl;
     cin >> nom;
+    cout << "Hola " << nom << "!" << endl;
+
 
 
     string arr_options[] = {"Sortir", "Benvinguda", "Redefinir el nom" };
     int opcio = 0;
 
-    for(int i = 0; i < 3; i++){
-        cout << i + 1 << " - " << arr_options[i] << endl;
-    }
-
     
-    cin >> opcio;
-    if(opcio == 1){
-        cout <<"Fins aviat " << nom << "!" << endl;
-        return 0;
-    }
-    
-
-
-
     do{
         for(int i = 0; i < 3; i++){
             cout << i + 1 << " - " << arr_options[i] << endl;
         }
         cin >> opcio;
+
+        switch (opcio)
+        {
+            case 1: cout << "Sortint" << endl;
+                break;
+            case 2: 
+                cout << "Benvingut a l'assignatura d'ED " << nom << endl;
+                break;
+            
+            case 3:
+                cout << "Introdueix el nou nom que vols posar" << endl;
+                cin >> nom;
+                cout << "El teu nom ha estat canviat a " << nom << endl;
+                break;
+            
+            default: cout << "Opció no vàlida" << endl; break;
+
+        }
 
     }while(opcio!= 1);
     
